071cc096821db97fcbbae3ef50fa6d5874d6ad21
tcasquero <tcasquero@gmail.com>
2013-10-14 23:28:16 +0200

    adding a lungo into chocolateTweet

--- chocolateTweet/platforms/ios/www/js/index.js
+++ chocolateTweet/platforms/ios/www/js/index.js
@@ -38,6 +38,6 @@ var app = {
     // Update DOM on a Received Event
     receivedEvent: function(id) {
         console.log('Received Event: ' + id);
-        angular.bootstrap(document,["myApp"]);
+        //angular.bootstrap(document,["myApp"]);
     }
 };
