aa999ae153598b2c98caa20a08ab9479eb65727c
Max Aldunate <maxaldunate@gmail.com>
2013-10-19 19:28:46 +0200

    ugly splash window

--- Prototypes/controllers/maxis.js
+++ Prototypes/controllers/maxis.js
@@ -1,13 +1,13 @@
 var RunForLifeApp = angular.module('RunForLifeApp', []);
  
 RunForLifeApp.controller('MainController', function MainController($scope) {
-  $scope.games = [
-    {'name': 'Run for your life'}
+  $scope.buttons = [
+    {'name': 'Crea Tu Perfil'}
   ];
 });
 
 RunForLifeApp.controller('PerfilController', function PerfilController($scope) {
-  $scope.games = [
+  $scope.fields = [
     {'name': 'Crea Tu Perfil'}
   ];
 });
