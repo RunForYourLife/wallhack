56a1462d24b45d641e62f9583f82bb929dc252af
jahzielHA <jahzielhidalgo@gmail.com>
2013-10-23 21:46:02 +0100

    Adding angular routing

--- Prototypes/controllers/run4.js
+++ Prototypes/controllers/run4.js
@@ -1,17 +1,33 @@
-var RunForLifeApp = angular.module('RunForLifeApp', []);
- 
+var RunForLifeApp = angular
+  .module('RunForLifeApp', ['ngRoute'])
+  .config(function($routeProvider, $locationProvider){
+
+      $locationProvider.html5Mode(false);
+
+      $routeProvider
+        .when('/Games/:gameId', {
+          templateUrl: 'GameDetails.html',
+          controller: 'GameDetailsController'
+        });
+  });
+
+
 RunForLifeApp.controller('GamesController', function GamesController($scope) {
   $scope.games = [
-    {'name': 'Run for your life',
+    {'id' : 1,
+     'name': 'Run for your life',
      'description': 'asdfas dasdf asdf asdf asdf '},
-    {'name': 'The Farm',
+    {'id' : 2,
+     'name': 'The Farm',
      'description': 'asdf asdf asd fasd f'},
-    {'name': 'Your mum trains with you',
+    {'id' : 3,
+     'name': 'Your mum trains with you',
      'description': 'asdf asd fasd fasd fasd fasd fasdf'}
   ];
 });
 
-RunForLifeApp.controller('GameDetailsController', function GameDetailsController($scope) {
+RunForLifeApp.controller('GameDetailsController', function GameDetailsController($scope, $routeParams) {
+  $scope.id = $routeParams
   $scope.name =  'Run for your life';
   $scope.description =  'asdfas dasdf asdf asdf asdf';
 });
